18a79ed88ccb5d871e72f67a130a8f50b41ee8ad
Russell Keppner <rkeppner@solardrive.io>
2023-03-20 13:39:13 -0600

    Add employee_id to user factory for seeding

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,6 +23,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'employee_id' => chr(rand(65,90)) . chr(rand(65,90)) . '-' .
+                str_pad(rand(0, pow(10, 4) -1), 4, '0', STR_PAD_LEFT),
         ];
     }
 
